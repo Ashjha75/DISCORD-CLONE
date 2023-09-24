af495949cf5e5037bbe59ed45ba6b55ea674b00b
AshishJha <ajha5645@gmail.com>
2023-09-24 12:45:42 +0530

    BASE COMMIT

---
+++ discordapp/app/(root)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex justify-center items-center bg-red-500 text-white">
+      <div className="font-bold">HELLO WORLD</div>
+    </main>
+  );
+}
